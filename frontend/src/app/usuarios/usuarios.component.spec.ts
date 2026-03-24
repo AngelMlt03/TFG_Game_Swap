23781b129adc53e170004aa9e74f125ee93be177
AngelMlt03 <angelmlt03@gmail.com>
2026-03-24 13:59:57 +0100

    fix: Arreglo test Angular no usado

--- frontend/src/app/usuarios/usuarios.component.spec.ts
+++ frontend/src/app/usuarios/usuarios.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsuariosComponent } from './usuarios.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('UsuariosComponent', () => {
   let component: UsuariosComponent;
@@ -8,7 +10,11 @@ describe('UsuariosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UsuariosComponent]
+      imports: [UsuariosComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     })
     .compileComponents();
 
